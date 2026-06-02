0dd2ce16d6a9d523056c9efc0b239112fc3db6bc
kkcrsp <victorccdsouza@gmail.com>
2026-06-02 19:56:07 -0300

    Delete soma.js

--- soma.js
+++
@@ -1,8 +0,0 @@
-function soma(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error('Os parâmetros devem ser números');
-    }
-    return a + b;
-}
-
-module.exports = soma;
